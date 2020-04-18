9ee183a8397931a0ed010146e03ca5fc92f594bf
Evan Smith <professionalemail@zoho.com>
2020-04-18 02:19:12 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,12 +8,11 @@ class MP3Importer
   end
   
   def files
-    Dir.glob("#{@path}/*.mp3").map {|mp3| mp3.gsub("#{path}/", "")}
+    @files = Dir.glob("#{@path}/*.mp3").map {|mp3| mp3.gsub("#{path}/", "")}
   end
   
-  def import(list)
-    songs = list.files
-    songs.each{|file| Song.new_by_filename(file) }
+  def import
+    @files.each{|file| Song.new_by_filename(file) }
     binding.pry
   end
 
